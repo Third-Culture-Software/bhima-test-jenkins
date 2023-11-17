ac5e6c86f3c208b239d6553d7364e729399d1f3d
Jenkins <jmcameron@gmail.com>
2023-11-17 10:30:21 +0100

    Added url

--- no-executors.groovy
+++ no-executors.groovy
@@ -1,2 +1,5 @@
+// For usage, see https://github.com/jenkinsci/docker/blob/master/README.md#setting-the-number-of-executors
+
 import jenkins.model.*
 Jenkins.instance.setNumExecutors(0) // Recommended to not run builds on the built-in node
+
